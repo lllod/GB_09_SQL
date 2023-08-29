bea98056a4fd5650cc5559d2d448cf8e65253fb7
lllod <ygo.admin@gmail.com>
2023-08-29 10:15:01 +0400

    update seminar3

--- seminar3/seminar3.sql
+++ seminar3/seminar3.sql
@@ -53,32 +53,95 @@ INSERT INTO orders VALUE
 # 1. Напишите запрос, который вывел бы таблицу со столбцами в следующем порядке: city, sname, snum, comm.
 # (к первой или второй таблице, используя SELECT)
 
-SELECT city, sname, snum, comm FROM salespeople;
+SELECT city, sname, snum, comm
+FROM salespeople;
 
 # 2. Напишите команду SELECT, которая вывела бы оценку(rating),
 # сопровождаемую именем каждого заказчика в городе San Jose. (“заказчики”)
 
-SELECT cname, rating FROM customers WHERE city = 'San Jose';
+SELECT cname, rating
+FROM customers
+WHERE city = 'San Jose';
 
 # 3. Напишите запрос, который вывел бы значения snum всех продавцов
 # из таблицы заказов без каких бы то ни было повторений. (уникальные значения в  “snum“ “Продавцы”)
 
-SELECT DISTINCT snum FROM orders;
+SELECT DISTINCT snum
+FROM orders;
 
 # 4*. Напишите запрос, который бы выбирал заказчиков, чьи имена начинаются с буквы G.
 # Используется оператор "LIKE": (“заказчики”)
 
-SELECT * FROM customers WHERE cname LIKE 'G%';
+SELECT *
+FROM customers
+WHERE cname LIKE 'G%';
 
 # 5. Напишите запрос, который может дать вам все заказы со значениями суммы выше чем $1,000.
 # (“Заказы”, “amt”  - сумма)
 
-SELECT * FROM orders WHERE amt > 1000;
+SELECT *
+FROM orders
+WHERE amt > 1000;
 
 # 6. Напишите запрос который выбрал бы наименьшую сумму заказа.
 #  (Из поля “amt” - сумма в таблице “Заказы” выбрать наименьшее значение)
 
-# SELECT * FROM orders WHERE min(amt);
+SELECT min(amt)
+FROM orders;
 
 # 7. Напишите запрос к таблице “Заказчики”, который может показать всех заказчиков,
 # у которых рейтинг больше 100 и они находятся не в Риме.
+
+SELECT *
+FROM customers
+WHERE rating > 100
+  and city != 'Rome';
+
+CREATE TABLE IF NOT EXISTS classwork
+    (id         INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name       VARCHAR(10)        NOT NULL,
+    surname    VARCHAR(20)        NOT NULL,
+    speciality VARCHAR(10)        NOT NULL,
+    seniority  SMALLINT           NOT NULL,
+    salary     INT                NOT NULL,
+    age        SMALLINT           NOT NULL);
+
+INSERT INTO classwork(name, surname, speciality, seniority, salary, age) VALUE
+    ('Вася', 'Васькин', 'начальник', 40, 100000, 60),
+    ('Петя', 'Петькин', 'начальник', 8, 70000, 30),
+    ('Катя', 'Каткина', 'инженер', 2, 70000, 25),
+    ('Саша', 'Сашкин', 'инженер', 12, 50000, 35),
+    ('Иван', 'Иванов', 'рабочий', 40, 30000, 59),
+    ('Петр', 'Петров', 'рабочий', 20, 25000, 40),
+    ('Сидор', 'Сидоров', 'рабочий', 10, 20000, 35),
+    ('Антон', 'Антонов', 'рабочий', 8, 19000, 28),
+    ('Юра', 'Юркин', 'рабочий', 5, 15000, 25),
+    ('Максим', 'Воронин', 'рабочий', 2, 11000, 22),
+    ('Юра', 'Галкин', 'рабочий', 3, 12000, 24),
+    ('Люся', 'Люськина', 'уборщик', 10, 10000, 49);
+
+# 1. Отсортируйте поле “зарплата” в порядке убывания и возрастания
+
+SELECT *
+FROM classwork
+ORDER BY salary;
+
+SELECT *
+FROM classwork
+ORDER BY salary DESC;
+
+# 2.** Отсортируйте по возрастанию поле “Зарплата” и
+# выведите 5 строк с наибольшей заработной платой (возможен подзапрос)
+
+SELECT *
+FROM classwork
+ORDER BY salary
+LIMIT 7, 5;
+
+# 3. Выполните группировку всех сотрудников по специальности,
+# суммарная зарплата которых превышает 100000
+
+SELECT speciality, sum(salary)
+FROM classwork
+GROUP BY speciality
+HAVING sum(salary) > 100000;
